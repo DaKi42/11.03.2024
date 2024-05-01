ca7a9404b513a97f990f224a71af7081dd91464c
Illia Bondar <kava.kav.23@gmail.com>
2024-05-01 20:01:42 +0200

    Add project files.

---
+++ SocketHelloWorld/Source.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#include <string>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include "resource.h"
+#include <tchar.h>
+#include <CommCtrl.h>
+#pragma comment(lib, "Ws2_32.lib")
+
+
+SOCKET _socket;
+
+BOOL CALLBACK DialogProc(HWND hwndDlg, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
+    case WM_INITDIALOG:
+        return TRUE;
+    case WM_COMMAND:
+        switch (LOWORD(wParam)) {
+        case IDC_BUTTON1: {
+            const int MAXSTRLEN = 255;
+            char buf[MAXSTRLEN];
+            char str[MAXSTRLEN];
+
+            // get text from idc_edit1
+            GetDlgItemText(hwndDlg, IDC_EDIT1, str, MAXSTRLEN);
+
+            // server connect
+            WSADATA wsaData;
+            if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+                MessageBox(NULL, "WSAStartup failed", "Error", MB_OK | MB_ICONERROR);
+                return TRUE;
+            }
+
+            _socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+            if (_socket == INVALID_SOCKET) {
+                MessageBox(NULL, "Socket creation failed", "Error", MB_OK | MB_ICONERROR);
+                WSACleanup();
+                return TRUE;
+            }
+
+            sockaddr_in addr;
+            addr.sin_family = AF_INET;
+            inet_pton(AF_INET, "127.0.0.1", &addr.sin_addr);
+            addr.sin_port = htons(20000);
+
+            if (connect(_socket, (SOCKADDR*)&addr, sizeof(addr)) == SOCKET_ERROR) {
+                MessageBox(NULL, "Connection failed", "Error", MB_OK | MB_ICONERROR);
+                closesocket(_socket);
+                WSACleanup();
+                return TRUE;
+            }
+
+            // send msg to server
+            send(_socket, str, strlen(str), 0);
+
+            // get server answer
+            int i = recv(_socket, buf, MAXSTRLEN, 0);
+            buf[i] = '\0';
+
+            MessageBox(NULL, buf, "Message from Server", MB_OK | MB_ICONINFORMATION);
+
+            // close socket and clean winsock
+            closesocket(_socket);
+            WSACleanup();
+
+            return TRUE;
+        }
+        }
+        break;
+    case WM_CLOSE:
+        EndDialog(hwndDlg, 0);
+        break;
+    }
+    return FALSE;
+}
+
+int WINAPI _tWinMain(HINSTANCE hInst, HINSTANCE hPrev, LPTSTR lpszCmdLine, int nCmdShow) {
+    return DialogBox(hInst, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DialogProc);
+}
